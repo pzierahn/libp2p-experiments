9aaeab3b1d6cfa6b5b1f503dd41f12828dc32dcd
Patrick Zierahn <5384625+pzierahn@users.noreply.github.com>
2022-09-22 18:08:12 +0200

    Check in

--- cmd/swarm/swarm.go
+++ cmd/swarm/swarm.go
@@ -7,13 +7,14 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 	"github.com/multiformats/go-multiaddr"
 	"log"
-	"p2p/discov"
 )
 
 var (
 	connect  = flag.String("connect", "", "connect to multipart address")
+	relayS   = flag.Bool("relay", false, "enable relay")
 	provider = flag.Bool("provider", false, "enable provider")
 	consumer = flag.Bool("consumer", false, "enable consumer")
 	port     = flag.Int("port", 0, "enable consumer")
@@ -26,7 +27,9 @@ func init() {
 
 func main() {
 
-	var opts []libp2p.Option
+	opts := []libp2p.Option{
+		libp2p.EnableRelay(),
+	}
 
 	if *port > 0 {
 		opts = append(opts, libp2p.ListenAddrStrings(
@@ -42,34 +45,33 @@ func main() {
 		log.Fatalf("Failed to create h1: %v", err)
 	}
 
-	for _, addr := range host.Addrs() {
-		hostAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/p2p/%s", host.ID()))
-		log.Printf("hostAddr=%v", addr.Encapsulate(hostAddr).String())
+	if *relayS {
+		_, err := relay.New(host)
+		if err != nil {
+			log.Fatalf("Failed to create relay: %v", err)
+		}
 	}
 
 	ctx := context.Background()
 
-	if *connect != "" {
-		maddr, err := multiaddr.NewMultiaddr(*connect)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	maddr, err := multiaddr.NewMultiaddr(*connect)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		info, err := peer.AddrInfoFromP2pAddr(maddr)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		log.Printf("connect to %+v", info)
+	log.Printf("connect to %+v", info)
 
-		err = host.Connect(ctx, *info)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	err = host.Connect(ctx, *info)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	if *provider {
-		discov.AddHost(host, "hasenfurz")
 		host.SetStreamHandler("test", func(stream network.Stream) {
 			log.Printf("test: recieved stream %v", stream.ID())
 			_, err = stream.Write([]byte("Hello from " + host.ID().String()))
@@ -80,22 +82,21 @@ func main() {
 	}
 
 	if *consumer {
-		discov.AddHost(host, "hasenfurz", func(addr peer.AddrInfo) {
-			err = host.Connect(ctx, addr)
-			if err != nil {
-				log.Fatalf("Failed to connect %v: %v", addr, err)
-			}
 
-			stream, err := host.NewStream(ctx, addr.ID, "test")
+		stream, err := host.NewStream(ctx, info.ID)
+		buf := make([]byte, 1024)
+		_, err = stream.Read(buf)
+		if err != nil {
+			log.Fatalf("Failed to read stream: %v", err)
+		}
 
-			buf := make([]byte, 1024)
-			_, err = stream.Read(buf)
-			if err != nil {
-				log.Fatalf("Failed to read stream %v: %v", addr, err)
-			}
+		log.Printf("message=%s", buf)
 
-			log.Printf("message=%s", buf)
-		})
+	}
+
+	for _, addr := range host.Addrs() {
+		hostAddr, _ := multiaddr.NewMultiaddr(fmt.Sprintf("/p2p/%s", host.ID()))
+		log.Printf("hostAddr=%v", addr.Encapsulate(hostAddr).String())
 	}
 
 	<-ctx.Done()
